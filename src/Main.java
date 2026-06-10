7f393c7e1022839d3bf67f08ff5dd4d63879d66f
Achintya Kaushal Jha <jhaachintya16@gmail.com>
2026-06-10 23:04:10 +0530

    storing list of expenses,method overriding of to string,understanding println,generics

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Expense e=new Expense(1,475.66f,"Food","Swiggy","09-06-2026");
-        e.toString();
+        /*Expense e=new Expense(1,475.66f,"Food","Swiggy","09-06-2026");
+        e.toString();*/
+        ArrayList<Expense> expenses=new ArrayList<>();
+        expenses.add(new Expense(1,388.6f,"Food","zomato","10-06-2026"));
+        expenses.add(new Expense(2,255,"ride","uber","08-06-2026"));
+        expenses.add(new Expense(3,580,"ration","swiggy","08-06-2026"));
+        System.out.println("list waala"+expenses);
+        for(Expense expense:expenses){
+            System.out.println(expense);
+        }
         //Expense.printExpense(e);
     }
 }
