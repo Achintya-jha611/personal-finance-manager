a59828c7a9422ecc636455243b390faaed5c4872
Achintya Kaushal Jha <jhaachintya16@gmail.com>
2026-06-18 01:39:18 +0530

    day-8 changes getting input from main and separating concerns for service and entity class

--- src/ExpenseService.java
+++ src/ExpenseService.java
@@ -10,20 +10,8 @@ public class ExpenseService {
         System.out.println("3. Exit");
         System.out.println("Enter choice");
     }
-    public  void addExpense(Scanner sc){
-        System.out.println("Enter id:");
-        int id = sc.nextInt();
-        System.out.println("Enter Amount:");
-        float amount = sc.nextFloat();
-        sc.nextLine();
-        System.out.println("Enter Category:");
-        String category = sc.nextLine();
-        System.out.println("Enter Description");
-        String description = sc.nextLine();
-        System.out.println("Enter Date:");
-        String date = sc.nextLine();
-        Expense e = new Expense(id, amount, category, description, date);
-        expenses.add(e);
+    public  void addExpense(Expense expense){
+        expenses.add(expense);
         System.out.println("Expense Added Successfully");
     }
     public  void viewExpense(){
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static Expense getExpenseFromUser(Scanner sc){
+        System.out.println("Enter id:");
+        int id = sc.nextInt();
+        System.out.println("Enter Amount:");
+        float amount = sc.nextFloat();
+        sc.nextLine();
+        System.out.println("Enter Category:");
+        String category = sc.nextLine();
+        System.out.println("Enter Description");
+        String description = sc.nextLine();
+        System.out.println("Enter Date:");
+        String date = sc.nextLine();
+        Expense expense = new Expense(id, amount, category, description, date);
+        return expense;
+    }
     public static void main(String[] args) {
         ExpenseService expenseService=new ExpenseService();
         //ArrayList<Expense> expenses=new ArrayList<>();
@@ -12,7 +27,8 @@ public class Main {
             expenseService.printMenu();
             int optionChosen=sc.nextInt();
             if (optionChosen == 1) {
-                expenseService.addExpense(sc);
+                Expense expense=getExpenseFromUser(sc);
+                expenseService.addExpense(expense);
                 System.out.println("Want to add more expenses?Enter 1 to continue and 2 to stop");
                 int choice=sc.nextInt();
                 if(choice==1){
